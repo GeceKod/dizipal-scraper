62afc4784761ce8c7907616cbe647916b0f97689
GeceKod <gecekod@aol.com>
2025-07-12 13:42:59 +0300

    Update dizipal.py

--- dizipal.py
+++ dizipal.py
@@ -10,6 +10,7 @@ from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Hash import SHA512
 import base64
 import logging
+import requests
 
 # Loglama ayarları
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
